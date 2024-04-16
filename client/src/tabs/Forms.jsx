3b85ffebde3c3800c74dc7cba3258339d4e2f642
r2pen2 <joedobbelaar@gmail.com>
2024-04-16 13:59:47 -0400

    Forms Embed

--- client/src/tabs/Forms.jsx
+++ client/src/tabs/Forms.jsx
@@ -1,27 +1,37 @@
-import { Blockquote, Paper, Tooltip } from '@mantine/core'
-import React from 'react'
+import { Blockquote, Button, Modal, Paper, Tooltip } from '@mantine/core'
+import React, { useRef, useState } from 'react'
 import "../assets/style/forms.css"
-import { IconAlertCircle, IconCheck, IconCircleCheck, IconCircleCheckFilled, IconHelpHexagonFilled, IconInfoCircle, IconQuestionMark } from '@tabler/icons-react'
+import { IconAlertCircle, IconCheck, IconChevronLeft, IconCircleCheck, IconCircleCheckFilled, IconHelpHexagonFilled, IconInfoCircle, IconQuestionMark } from '@tabler/icons-react'
 
-const forms = [
-  {
-    title: "Client Intake Form",
-    description: "Basic information about the client: name, address, phone number, etc.",
-    complete: false,
-  },
-  {
-    title: "About Me Form",
-    description: "Information about the client's background, family, and other personal details.",
-    complete: true,
-  },
-]
+const formHeight = 500
+const formWidth = 6404
 
 export default function Forms() {
   
+  const exampleForm = <iframe title="Example Form" src={"https://docs.google.com/forms/d/e/1FAIpQLScE6pUzztBY-mbD6NrQl6R-qeDLAJOp2_QWR0uUR7NyMu_TJg/viewform?embedded=true"} width={formWidth} height={formHeight} frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  
+  const forms = {
+    CLIENTINTAKE: {
+      title: "Client Intake Form",
+      description: "Basic information about the client: name, address, phone number, etc.",
+      complete: false,
+      iframe: exampleForm,
+    },
+    ABOUTME: {
+      title: "About Me Form",
+      description: "Information about the client's background, family, and other personal details.",
+      complete: true,
+      iframe: exampleForm,
+    }
+  }
+
   const FormsList = () => {
+
+    if (currentForm) { return; }
+
     return (
-      forms.map((form, index) => 
-      <Paper key={index} className="col-6 col-sm-12 mb-2 p-2 form-paper d-flex flex-row align-items-center justify-content-between" withBorder padding="md" shadow="xs" style={{ width: "100%" }}>
+      Object.values(forms).map((form, index) => 
+      <Paper key={index} onClick={() => setCurrentForm(form)} className="col-6 col-sm-12 mb-2 p-2 form-paper d-flex flex-row align-items-center justify-content-between" withBorder padding="md" shadow="xs" style={{ width: "100%" }}>
         <div className="form-title">
           <h3>{form.title}</h3>
           <p>{form.description}</p>
@@ -33,15 +43,29 @@ export default function Forms() {
     )
   }
 
+  const [currentForm, setCurrentForm] = React.useState(null)
+
+  const CurrentForm = () => {
+    if (!currentForm) { return; }
+
+    return [
+      <div className="d-flex flex-row">
+        <Button onClick={() => setCurrentForm(null)} leftSection={<IconChevronLeft />}>Back to All Forms</Button>
+      </div>,
+      currentForm.iframe
+    ];
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center flex-column">
-        <h2>Forms</h2>
-        <p>This is a list of all forms assigned to a given client The client can use this page to fill them out for the first time, confirm that they've been filled, and view them at any point.</p>
+        <h2>{currentForm ? currentForm.title : "Forms"}</h2>
+        {!currentForm && <p>This is a list of all forms assigned to a given client The client can use this page to fill them out for the first time, confirm that they've been filled, and view them at any point.</p>}
       </div>
       <div className="container">
         <div className="row">
           <FormsList />
+          <CurrentForm />
         </div>
       </div>
       
