06a0c57a32ba3b40a91121b5de2f7eae64baf4f8
r2pen2 <joedobbelaar@gmail.com>
2024-05-15 14:15:46 -0400

    dashboard intro

--- client/src/tabs/Dashboard.jsx
+++ client/src/tabs/Dashboard.jsx
@@ -1,7 +1,8 @@
-import { Paper } from '@mantine/core'
+import { Paper, Table } from '@mantine/core'
 import React from 'react'
 
 import { tools } from '../api/tools.ts'
+import { getSlashDateString } from '../api/strings.js'
 
 export default function Dashboard() {
   
@@ -18,6 +19,8 @@ export default function Dashboard() {
       <div className="d-flex align-items-center flex-column">
         <h2>Dashboard</h2>
       </div>
+      <h3>Upcoming Assignments</h3>
+      <Tracker />
       <h3>My Tools</h3>
       <ToolsList />
     </div>
@@ -32,3 +35,47 @@ const ToolCard = ({tool}) => (
     </Paper>
   </div>
 )
+
+const Tracker = () => {
+  return (
+    
+    <Table.ScrollContainer minWidth={500} type="native">
+    <Table striped>
+      <Table.Thead>
+        <Table.Th>
+          Subject
+        </Table.Th>
+        <Table.Th>
+          Assignment
+        </Table.Th>
+        <Table.Th>
+          Status
+        </Table.Th>
+        <Table.Th>
+          Est Time
+        </Table.Th>
+        <Table.Th>
+          Priority
+        </Table.Th>
+        <Table.Th>
+          Start Date
+        </Table.Th>
+        <Table.Th>
+          Due Date
+        </Table.Th>
+      </Table.Thead>
+      <Table.Tbody>
+        <Table.Tr key={1}>
+          <Table.Td>English 2000</Table.Td>
+          <Table.Td>Read Intro, 1-11 and 18-19  and Chapter 1, 21-32</Table.Td>
+          <Table.Td>Done</Table.Td>
+          <Table.Td>2 Hours</Table.Td>
+          <Table.Td>Low</Table.Td>
+          <Table.Td>{getSlashDateString(new Date())}</Table.Td>
+          <Table.Td>{getSlashDateString(new Date())}</Table.Td>
+        </Table.Tr>
+      </Table.Tbody>
+    </Table>
+  </Table.ScrollContainer>
+  )
+}
