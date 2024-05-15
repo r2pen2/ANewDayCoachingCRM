a35839a0c66a35daa2a454c5baef2359ee32d369
r2pen2 <joedobbelaar@gmail.com>
2024-05-14 22:31:00 -0400

    firebase, maybe?

--- routes/forms.js
+++ routes/forms.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const db = require('../firebase');
 
 router.use(bodyParser.json());
 
 router.post("/submitted", (req, res) => {
   const formId = req.body.formId;
   const userEmail = req.body.userEmail;
-  console.log(formId, userEmail);  
+  db.collection("users").where("personalData.email", "==", userEmail).get().then((querySnapshot) => {
+    querySnapshot.docs.forEach((doc) => {
+      const user = doc.data();
+      if (user.forms) {
+        user.forms.push(formId);
+      } else {
+        user.forms = [formId];
+      }
+      db.collection("users").doc(doc.id).set(user);
+    });
+  });
 })
 
 module.exports = router;
