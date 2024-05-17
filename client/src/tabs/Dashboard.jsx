00bbf5e5dc936df04b3794e86067a59956e0af30
r2pen2 <joedobbelaar@gmail.com>
2024-05-17 19:19:06 -0400

    favoriting tools... maybe this is useful?

--- client/src/tabs/Dashboard.jsx
+++ client/src/tabs/Dashboard.jsx
@@ -1,17 +1,28 @@
-import { Paper, Table } from '@mantine/core'
+import { Paper, Table, Tooltip } from '@mantine/core'
 import React from 'react'
+import { Carousel } from '@mantine/carousel';
+import '@mantine/carousel/styles.css';
+
+import "../assets/style/dashboard.css"
 
 import { tools } from '../api/tools.ts'
 import { getSlashDateString } from '../api/strings.js'
+import { IconStar } from '@tabler/icons-react';
 
 export default function Dashboard() {
   
   const ToolsList = () => (
-    <div className="container-fluid">
-      <div className="row">
-        {tools.map((tool, index) => <ToolCard key={index} tool={tool} />)}
-      </div>
-    </div>
+    <Carousel
+      slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
+      slideGap={{ base: 0, sm: 'md' }} 
+      withIndicators
+      loop
+      withControls={false}
+      dragFree
+      className="indicator-container"
+    >
+      {tools.map((tool, index) => <ToolCard key={index} tool={tool} />)}
+    </Carousel>
   )
 
   return (
@@ -27,14 +38,38 @@ export default function Dashboard() {
   )
 }
 
-const ToolCard = ({tool}) => (
-  <div className="col-6 col-md-4 col-lg-3 mb-2 ">
-    <Paper withBorder className="p-2 h-100" style={{marginRight: "1rem", minWidth: 200}}>
-      <strong>{tool.title}</strong>
-      <p >{tool.description}</p>
-    </Paper>
-  </div>
-)
+const ToolCard = ({tool}) => {
+  
+  function starTool() {
+    console.log(`Starting ${tool.title}`)
+    tool.starred = !tool.starred;
+  }
+
+  function getLabel() {
+    return (tool.starred ? "Unfavorite" : "Favorite") + ` "${tool.title}"`
+  }
+
+  return (
+    <Carousel.Slide style={{marginTop: "1rem", marginBottom: "1rem"}}>
+    <Paper
+        withBorder 
+        className={"p-2 h-100"} 
+        style={{
+          marginRight: "1rem",
+          filter: tool.starred ? "drop-shadow(0 0 0.5rem gold)" : "none",
+        }}
+      >
+        <div className="d-flex justify-content-between">
+          <strong>{tool.title}</strong>
+          <Tooltip label={getLabel()} onClick={starTool}>
+            <IconStar className='favorite-button' fill={tool.starred ? "gold" : "transparent"} stroke={tool.starred ? "gold" : "black"} />
+          </Tooltip>
+        </div>
+        <p>{tool.description}</p>
+      </Paper>
+    </Carousel.Slide>
+  )
+}
 
 const Tracker = () => {
   return (
