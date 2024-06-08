212714ca5094232109bdaa6d12b070913b440237
r2pen2 <joedobbelaar@gmail.com>
2024-06-08 17:30:37 -0400

    server search route

--- routes/users.js
+++ routes/users.js
@@ -48,6 +48,21 @@ router.get("/search-tools", (req, res) => {
   res.json(resUsers);
 })
 
+router.get("/search-users", (req, res) => {
+  const resUsers = {}
+  for (const userId of Object.keys(allUsers)) {
+    const u = allUsers[userId];
+    resUsers[userId] = {
+      personalData: {
+        displayName: u.personalData.displayName,
+        email: u.personalData.email
+      },
+      id: u.id
+    }
+  }
+  res.json(resUsers);
+})
+
 function getAllUsers() { return allUsers }
 function getUser(id) { return allUsers[id]; }
 async function setUser(user) { return new Promise((resolve, reject) => { db.collection("users").doc(user.id).set(user).then(() => { resolve(); }).catch((error) => { reject(error); }); }) }
