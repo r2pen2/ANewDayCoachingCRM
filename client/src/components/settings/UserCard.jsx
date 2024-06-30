928d31d9d6e0bc694b54e76c0f9b52e7c6c06cf8
r2pen2 <joedobbelaar@gmail.com>
2024-06-30 17:17:04 -0400

    sign-out takes user to dashboard on sign-in

--- client/src/components/settings/UserCard.jsx
+++ client/src/components/settings/UserCard.jsx
@@ -8,13 +8,19 @@ export const UserCard = memo(function UserCard({user}) {
 
   const SettingsAvatar = () => user.pfpUrl ? <Avatar mx="auto" src={user.pfpUrl} alt={user.displayName} size={120} style={{marginBottom: "1rem"}} /> :  <Avatar mx="auto" alt={user.displayName} size={120} style={{marginBottom: "1rem"}}>{getInitials(user.displayName)}</Avatar>
 
+  const handleSignOut = () => {
+    auth.signOut().then(() => {
+      window.location.hash = "dashboard"
+    })
+  }
+
   return (
     <div className="col-12 col-md-6 col-lg-4 col-xxl-2 px-0 px-md-2 mb-2 ">
       <Paper withBorder p="lg" className="d-flex flex-column top-green align-items-center" bg="var(--mantine-color-body)">
         <SettingsAvatar />
         <Text ta="center" fz="lg" fw={500} mt="md">{user.displayName}</Text>
         <Text ta="center" fz="sm" c="dimmed">{user.role}</Text>
-        <Button mt="md" onClick={() => auth.signOut()}>Sign Out</Button>
+        <Button mt="md" onClick={handleSignOut}>Sign Out</Button>
       </Paper>
     </div>
   )
