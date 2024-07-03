ae35d6a66f132e697339c54b749679450ecd849d
r2pen2 <joedobbelaar@gmail.com>
2024-07-03 12:19:08 -0400

    docs api??

--- routes/document.js
+++ routes/document.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const router = express.Router();
+require("dotenv").config()
+
+const serviceAccountKey = require("../config/cal.json")
+
+const { google } = require('googleapis');
+
 const bodyParser = require('body-parser');
-const { getUser, setUser } = require('./users');
 
 router.use(bodyParser.json());
 
+
 router.get("/", (req, res) => {
+  
+  const docs = google.docs('v1');
+
   const documentId = req.query.id
-  fetch(`https://www.googleapis.com/drive/v3/files/${documentId}?fields=name,mimeType`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer YOUR_ACCESS_TOKEN` // Replace with your actual access token
-    }
-  }).then(response => {
-    response.json().then(data => {
-      res.json(data);
-    })
-  });
+
+  docs.documents.get(documentId).then(doc => {
+    res.json(doc)
+  })
 })
+
 
 module.exports = router;
