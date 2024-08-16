5248f5a4dd982fb3df7005b4f8bff67885fe3c5c
r2pen2 <joedobbelaar@gmail.com>
2024-08-16 13:18:19 -0400

    header bottom margin

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export function AppShellHeader({burgerOpen, setBurgerOpen}) {
       <Group h="100%" px="md">
         <Burger opened={burgerOpen} onClick={() => setBurgerOpen(!burgerOpen)} hiddenFrom="lg" size="sm" />
         <img src={logo} alt="Logo" style={{ height: 40 }} />
-        <h1 style={{fontSize: "1.5rem"}} className="raleway-regular">Bluprint</h1>
+        <h1 style={{fontSize: "1.5rem", marginBottom: 0}} className="raleway-regular">Bluprint</h1>
       </Group>
     </AppShell.Header>
   )
